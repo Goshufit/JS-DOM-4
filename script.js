22b365619c90e340d4ec8aa491dc5576b79c9c4f
Jean-Pierre Graham <91097729+Goshufit@users.noreply.github.com>
2022-06-16 01:15:45 -0400

    Finalize Project

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ let itemList = document.getElementById('items');
 //Form submit event//
 form.addEventListener('submit', addItem);
 
+//Delete event
+itemList.addEventListener('click', removeItem);
+
+//Filter event
+filter.addEventListener('keyup', filterItems);
+
 //Add item
 function addItem(e){
     e.preventDefault();
@@ -20,5 +26,45 @@ li.className = 'list-group-item';
 //Add text node with input value
 li.appendChild(document.createTextNode(newItem));
 
+//Createdelete button element
+let deleteBtn = document.createElement('button');
+
+//Add classes to delete button
+deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+//Append text node
+deleteBtn.appendChild(document.createTextNode('X'));
+
+//Append button to li
+li.appendChild(deleteBtn)
+
+//Append li to list
 itemList.appendChild(li);
+}
+
+//Remove Item
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are You Sure?')){
+            let li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
+
+// Filter Items
+function filterItems(e){
+    // convert text to lowercase
+    let text = e.target.value.toLowerCase();
+    //Get li's
+    let items = itemList.getElementsByTagName('li');
+    //Convert to an array
+    Array.from(items).forEach(function(item){
+        let itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    })
 }
